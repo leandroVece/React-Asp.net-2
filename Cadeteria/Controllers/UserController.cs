209331bad213ae1315f985a49d20522f3d70854a
Leandro Suarez Vece <leovece1@gmail.com>
2023-07-18 11:07:52 -0300

    imagenes subidas

---
+++ Cadeteria/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+namespace Cadeteria.Controllers;
+
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Cadeteria.Authorization;
+using Cadeteria.Models;
+using Cadeteria.Services;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private IUserRepository _userRepository;
+    private IMapper _mapper;
+    private readonly AppSettings _appSettings;
+
+    public UserController(
+        IUserRepository userService,
+        IMapper mapper,
+        IOptions<AppSettings> appSettings)
+    {
+        _userRepository = userService;
+        _mapper = mapper;
+        _appSettings = appSettings.Value;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("authenticate")]
+    public IActionResult Authenticate(AuthenticateRequest model)
+    {
+        var response = _userRepository.Authenticate(model);
+        return Ok(response);
+    }
+
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest model)
+    {
+        _userRepository.Register(model);
+        return Ok(new { message = "Registration successful" });
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var users = _userRepository.GetAll();
+        return Ok(users);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(Guid id)
+    {
+        var user = _userRepository.GetById(id);
+        return Ok(user);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(Guid id, UpdateRequest model)
+    {
+        _userRepository.Update(id, model);
+        return Ok(new { message = "User updated successfully" });
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        _userRepository.Delete(id);
+        return Ok(new { message = "User deleted successfully" });
+    }
+}
--- Cadeteria/Program.cs
+++ Cadeteria/Program.cs
@@ -1,8 +1,34 @@
+using Cadeteria;
+using Cadeteria.Authorization;
+using Cadeteria.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+// if (env.IsProduction())
+//     builder.Services.AddDbContext<DataContext>();
+// else
+//     builder.Services.AddDbContext<DataContext, DataContext>();
+
+builder.Services.AddSqlServer<DataContext>(builder.Configuration.GetConnectionString("SQLServer"));
+
+builder.Services.AddCors();
+builder.Services.AddControllers();
+
+// configure automapper with all automapper profiles from this assembly
+builder.Services.AddAutoMapper(typeof(Program));
+
+// configure strongly typed settings object
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
+
+builder.Services.AddScoped<IJwtUtils, JwtUtils>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 
 var app = builder.Build();
 
@@ -11,6 +37,28 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+    dataContext.Database.Migrate();
+}
+
+{
+    // global cors policy
+    app.UseCors(x => x
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+
+    // global error handler
+    app.UseMiddleware<ErrorHandlerMiddleware>();
+
+    // custom jwt auth middleware
+    app.UseMiddleware<JwtMiddleware>();
+
+    app.MapControllers();
 }
 
 app.UseHttpsRedirection();
@@ -22,6 +70,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
-app.MapFallbackToFile("index.html");;
+app.MapFallbackToFile("index.html"); ;
 
 app.Run();
